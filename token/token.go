f0401141bd6f70ea03502f5a9fdc106c3ac83a83
Darrell-Devana <darrell.putra@binus.ac.id>
2026-05-02 11:30:08 +0700

    feat: add sketch for token.go

--- token/token.go
+++ token/token.go
@@ -1,1 +1,42 @@
 package token
+
+type TokenType int
+
+type Token struct {
+	Type    TokenType
+	Literal string
+	Line    int
+	Column  int
+}
+
+const (
+	IDENTIFIER TokenType = iota // 0
+	INT                         // 1
+
+	// Keywords
+	LET // 2
+
+	// Operators
+	ASSIGN // 3
+	PLUS   // 4
+
+	// Delimiters
+	COMMA     // 5
+	SEMICOLON // 6
+	LPAREN    // 7
+	RPAREN    // 8
+	LBRACE    // 9
+	RBRACE    // 10
+)
+
+var Keywords = map[string]TokenType{
+	"let": LET,
+}
+
+func isKeyword(str string) TokenType {
+	if keyword, ok := Keywords[str]; ok {
+		return keyword
+	} else {
+		return IDENTIFIER
+	}
+}
